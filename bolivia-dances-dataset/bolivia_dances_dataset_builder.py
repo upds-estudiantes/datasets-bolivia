d16ab25e400c1f971ac4e105df84d140a0109e7d
dariast03 <danteariastarifa@gmail.com>
2024-06-03 23:16:05 -0400

    chore: Update bolivia-dances-dataset builder and add dataset download functionality

--- bolivia-dances-dataset/bolivia_dances_dataset_builder.py
+++ bolivia-dances-dataset/bolivia_dances_dataset_builder.py
@@ -1,8 +1,9 @@
 """my_dataset dataset."""
 import os
-
+import zipfile
+import requests
 import tensorflow_datasets as tfds
-
+import gdown
 
 class Builder(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for my_dataset dataset."""
@@ -13,8 +14,9 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     }
 
     TRAIN_URL = "data/train"
-    TEST_URL = "data/test"
-
+    DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1UPg5K2Q-w_ImJ60FtDAejuFgQO66tUyp"
+    EXTRACTED_PATH = "extracted_data"
+    
     LABELS = ['Caporales', 'Carnavalito', 'Chacarera', 'Champas de Aiquile', 'Chovena', 'Chuncho', 'Cueca', 'Cueca Cochabambina', 'Diablada', 'El Torito', 'Lichiwayus de Ayopaya vestimenta', 'Macheteros', 'Morenada', 'Pim Pim', 'Potolos', 'Salay', 'Taquirari', 'Tinku', 'Tobas']
 
     IMAGE_SHAPE = (None, None, 3)
@@ -32,27 +34,35 @@ class Builder(tfds.core.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Returns SplitGenerators."""
-        # Download the data as specified in `_data_url` and write it to `downloaded_path`.
-        #!path = dl_manager.download_and_extract('https://data-url')
+        zip_path = os.path.join(dl_manager.download_dir, 'bolivia_dances.zip')
+        if not os.path.exists(zip_path):
+            gdown.download(self.DOWNLOAD_URL, zip_path, quiet=False)
+            
+        extracted_data_path = os.path.join(dl_manager.download_dir, self.EXTRACTED_PATH)
 
-        #ruta de los datos
-
+        if not os.path.exists(extracted_data_path):
+            os.makedirs(extracted_data_path)
+            
+            
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extracted_data_path)
+        
         return {
-            #!'train': self._generate_examples(path / 'train_imgs'),
-
-            #generar ejemplos tipos de datos (train, test, val)
-            'train': self._generate_examples(self.TRAIN_URL),
+            'train': self._generate_examples(extracted_data_path),
         }
 
     def _generate_examples(self, path):
-        for folder in os.listdir(path):
-            folder_path = os.path.join(path, folder)
+        if os.path.isdir(path):
+            for folder in os.listdir(path):
+                folder_path = os.path.join(path, folder)
 
-            if os.path.isdir(folder_path):
-                for file_name in os.listdir(folder_path):
-                    image_path = os.path.join(folder_path, file_name)
-                    image_id = "%s_%s" % (folder, file_name)
-                    yield image_id, {
-                        'image': image_path,
-                        'label': folder,
-                    }
+                if os.path.isdir(folder_path):
+                    for file_name in os.listdir(folder_path):
+                        image_path = os.path.join(folder_path, file_name)
+                        image_id = "%s_%s" % (folder, file_name)
+                        yield image_id, {
+                            'image': image_path,
+                            'label': folder,
+                        }
+        else:
+            raise ValueError(f"Expected a directory at {path}, but found a file. Please check the extraction process.")
